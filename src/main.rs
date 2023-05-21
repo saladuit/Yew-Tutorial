6b222f6fd452f57629eba7539dc5c79268fe9393
safoh <safoh@student.codam.nl>
2023-05-21 16:33:47 +0200

    Dependency init

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-use yew::prelude::*;
+use hello_world_in_rust::App;
 
-
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <div>
-            <h1>{ "Hello World!" }</h1>
-            <p>{ "This is my first yew app!" }</p>
-        </div>
-    }
-}
 
 fn main() {
     yew::Renderer::<App>::new().render();
